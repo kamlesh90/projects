2c0cea4fee9b40078119815c03d97221499d78ed
kamlesh.stech <kamlesh.stech@gmail.com>
2023-11-06 18:11:24 +0530

    multi module microservice project first commit

--- microservie-new/product-service/src/main/java/com/few/ps/dao/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.few.ps.dao;
-
-import com.few.ps.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-
-}
