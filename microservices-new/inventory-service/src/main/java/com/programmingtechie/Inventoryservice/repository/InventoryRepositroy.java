c8d425ed4ecde73a3a663395b25cccf2bcb81e15
kamlesh.stech <kamlesh.stech@gmail.com>
2023-11-07 17:11:20 +0530

    implement intercommunication between services

--- microservices-new/inventory-service/src/main/java/com/programmingtechie/Inventoryservice/repository/InventoryRepositroy.java
+++ microservices-new/inventory-service/src/main/java/com/programmingtechie/Inventoryservice/repository/InventoryRepositroy.java
@@ -4,11 +4,15 @@ import com.programmingtechie.Inventoryservice.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface InventoryRepositroy extends JpaRepository<Inventory, Long> {
-    Optional<Inventory> findBySkuCode(String skuCode);
-//    @Query("")
-//    Optional<Inventory> findBySkuCode(String skuCode);
+
+    //@Query("select i from Inventory i WHERE i.skuCode IN (:skuCode)")
+    List<Inventory> findBySkuCodeIn(List<String> skuCodes);
+
+    //@Query("select skuCode from Inventory where skuCode =: skuCode")
+    //Optional<Inventory> findBySkuCode(String skuCode);
 
 }
